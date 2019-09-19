24649b29dce13803686c36c1fa0ee8cbdf95f4f4
Mustafa <hazim6163@gmail.com>
2019-09-19 23:24:53 +0200

    users route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 //body parser middleware
 app.use(bodyParser.json())
 
+//Routes :
+const usersRoute = require('./routes/users');
+app.use('/users', usersRoute);
+
 app.listen(3000, () => {
     console.log("Server working on Port: 3000");
 });
---
+++ routes/users.js
@@ -0,0 +1,112 @@
+const router = require('express').Router();
+const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+//Data validate Schemas
+userRegisterValidate = new Joi.object().keys({
+    name:Joi.string().min(5).max(30).required(),
+    username:Joi.string().alphanum().min(5).max(30).required(),
+    email:Joi.string().email().required(),
+    password:Joi.string().min(5).max(30).required(),
+    vKey:Joi.number().required(),
+    admin:Joi.number().required(),
+    photoUrl:Joi.string().allow('')
+});
+
+userLoginValidate = new Joi.object().keys({
+    username:Joi.string().alphanum().min(5).max(30).required(),
+    email:Joi.string().email().required(),
+    password:Joi.string().min(5).max(30).required()
+});
+
+
+//Register
+router.post('/register', async (req, res) => {
+
+    // to validate the user data:
+    const { error } = Joi.validate(req.body, userRegisterValidate);
+    //check if there an error with the validate:
+    if (error) {
+        //extract the error message from the error resp if the data is invalid
+        return res.status(406).json({error:error.details[0].message});
+    }
+
+    //check if the user already registered  , 
+    const userRegisteredByEmail = await User.findOne({ email: req.body.email });
+    const userRegisteredByUsername = await User.findOne({ username: req.body.username });
+    if (userRegisteredByEmail) {
+        return res.status(400).json({error:'user already registered'});
+    }
+    if (userRegisteredByUsername) {
+        return res.status(400).json({error:'username unavailable'});
+    }
+
+    //crypt the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        vKey: req.body.vKey,
+        admin: req.body.admin,
+        photoUrl: req.body.photoUrl
+    });
+    //save the user to the database 
+    try {
+        const newUser = await user.save();
+        var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        res.status(201).json({token: token});
+    } catch (err) {
+        res.status(400).json({error:'user cant be registered'});
+    }
+
+});
+
+//LOGIN :
+router.post('/login', async (req, res) => {
+
+    //validate the inputs 
+    const { error } = Joi.validate(req.body, userLoginValidate);
+    //check if there an error :
+    if (error) {
+        //extract the error message from the error resp if the data is invalid
+        return res.status(406).json({error:error.details[0].message});
+    }
+
+
+    //Login vars:
+    var usernameOrEmail, password, user, hashedPassword, isLoggedIn;
+
+    password = req.body.password;
+    usernameOrEmail = req.body.usernameOrEmail;
+
+    //try to find the user in the database
+    user = await User.findOne(
+        { $or: [
+            { username: usernameOrEmail },
+            { email: usernameOrEmail }
+            ]
+        }
+    );
+    if (!user) {
+        return res.status(400).json({error:'user can\'t be found.'});
+    }
+    hashedPassword = user.password;
+    isLoggedIn = await bcrypt.compare(password, hashedPassword);
+    if (!isLoggedIn) {
+        return res.status(400).json({error:'incorrect password'})
+    }
+
+    // setting up the token for the login
+    var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.status(200).json({token: token});
+
+});
+
+module.exports = router;
