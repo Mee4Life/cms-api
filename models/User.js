4c23bfddb420d75116fd2d7df627ef5ddd189c72
Mustafa <hazim6163@gmail.com>
2019-09-19 22:43:45 +0200

    create user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const mongooseTimeStamp = require('mongoose-timestamp');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    vKey: {type: String, required: true},
+    admin: {type: Number, required: true},
+    photoUrl: {type: String, required: false}
+});
+
+UserSchema.plugin(mongooseTimeStamp);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
